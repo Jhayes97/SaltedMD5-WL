e3bc1016c8ead2f4ecf8c7398678276a2422ba8d
Jhayes97 <49526650+Jhayes97@users.noreply.github.com>
2019-04-11 11:05:00 -0400

    Add files via upload

---
+++ WL.py
@@ -0,0 +1,16 @@
+import hashlib
+
+saltPrefix = "FCC"
+saltSuffix = "Cyber"
+hashedPass = "0f28845d31dbabc9ba6bab4f06cdc11a"
+dictionaryAbsoluteFilePath = '/home/clay/Documents/Brute/rockyou.txt'
+
+
+f = open(dictionaryAbsoluteFilePath,'r',encoding='utf-8')
+lines = f.read().splitlines()
+
+for line in lines: 
+    saltedString = saltPrefix + line + saltSuffix
+    x = hashlib.md5((saltedString).encode('utf-8')).hexdigest()
+    if x.lower() == hashedPass.lower():
+        print("The password is: " + line)
